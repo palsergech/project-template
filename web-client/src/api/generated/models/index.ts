6cbfdf915f5fc827c3826309d271dc9b32a21d42
pavelpiankov <pasha@outstream.tech>
2025-10-01 14:05:26 +0700

    commit added generated api files

---
+++ web-client/src/api/generated/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthErrorDTO';
+export * from './BadRequestErrorDTO';
+export * from './DomainErrorDTO';
+export * from './ErrorResponseDTO';
+export * from './GetGreetingDefaultResponse';
+export * from './NotFoundErrorDTO';
+export * from './NotFoundErrorDTODetails';
+export * from './ServerErrorDTO';
